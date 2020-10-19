b3cdbb94e288f2d36af592689007711e37e76368
Reshi <juancdominici@gmail.com>
2020-10-19 14:28:51 -0300

    Updates

---
+++ precache-manifest.f27e742af37df79c20d974066b08f224.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cd54b2b73ae05270c590ee9d2f9fcaec",
+    "url": "/yeezy-quote-machine/index.html"
+  },
+  {
+    "revision": "223928228990ec650b91",
+    "url": "/yeezy-quote-machine/static/css/main.c6f88d4b.chunk.css"
+  },
+  {
+    "revision": "549e7e467b59d86be8dd",
+    "url": "/yeezy-quote-machine/static/js/2.45a8c064.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/yeezy-quote-machine/static/js/2.45a8c064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "223928228990ec650b91",
+    "url": "/yeezy-quote-machine/static/js/main.a6cb51fa.chunk.js"
+  },
+  {
+    "revision": "4ef9fab7299793d8c768",
+    "url": "/yeezy-quote-machine/static/js/runtime-main.a3bdeefa.js"
+  },
+  {
+    "revision": "54594856b13b2433baea43640cd8a984",
+    "url": "/yeezy-quote-machine/static/media/SandsGreyhoodSeven.54594856.otf"
+  }
+]);
